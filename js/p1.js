46f360ca25d6500ab265211bd9d49546cf2ba996
Unknown <projektty.wos@gmail.com>
2018-03-15 19:15:35 +0100

    c2
    c2

---
+++ js/p1.js
@@ -0,0 +1,22 @@
+
+console.log("siema");
+ var str = Math.random();
+ 
+ str=parseInt(str*60);
+ 
+ for(var i=0;i<6;i++)
+ {	 
+ 
+ 
+ }
+ 
+ if(str = 5)
+	 
+ 
+ {
+	 
+	 
+ }
+console.log(str);
+
+https://github.com/Michallahcim/Pierwszy/invitations
