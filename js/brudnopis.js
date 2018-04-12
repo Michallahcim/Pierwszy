59875d6828887eb1b00bc64b8920cebd3b5311d7
Unknown <projektty.wos@gmail.com>
2018-04-12 19:21:46 +0200

    12.04

--- js/brudnopis.js
+++ js/brudnopis.js
@@ -65,3 +65,27 @@ var array2 = [0,0,0,0,0,0,0];
       array2[6]++;                 // jeśli liczba trafiałby się z jakiegoś innego przedziału niż te wyżej to w tabeli to w ostatnie kolumnie zwiększy się wartość
     }
     console.log("Test 2:",array2); 
+
+    var dice = document.getElementById('p1');
+dice.addEventListener('click', function()
+{
+
+    var j2 = (Math.random()*10);  
+    if (j2<10/6)                 
+    document.getElementById('k').innerHTML = '<img src="images/01.svg" class="kostka">';                 
+    else if (j2<2*10/6)          
+    document.getElementById('k').innerHTML = '<img src="images/02.svg" class="kostka">';   
+    else if (j2<3*10/6)
+    document.getElementById('k').innerHTML = '<img src="images/03.svg" class="kostka">';   
+    else if (j2<4*10/6)
+    document.getElementById('k').innerHTML = '<img src="images/04.svg" class="kostka">';   
+    else if (j2<5*10/6)
+    document.getElementById('k').innerHTML = '<img src="images/05.svg" class="kostka">';   
+    else if (j2<6*10/6)          
+    document.getElementById('k').innerHTML = '<img src="images/06.svg" class="kostka">';  
+    else
+    console.log("błąd");
+    
+
+    console.log("działa");
+})
