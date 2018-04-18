c95492fed8268e5bac9266ca774f431776d0c7d1
Krzysztof Olechno <krolechno@wp.pl>
2018-04-19 00:52:08 +0200

    19.04

--- js/p1.js
+++ js/p1.js
@@ -1,17 +1,23 @@
+var ctyp = '';
+
 var dice = document.getElementById('p1');
 dice.addEventListener('click', function(){
 kosc = parseInt((Math.random()*6)+1);
-document.getElementById('k').innerHTML = '<img src="images/0' + kosc + '.svg" class="kostka">';})
+document.getElementById('k').innerHTML = '<img src="images/0' + kosc + ctyp + '.svg" class="kostka">';})
+
 
 var rad1 =document.getElementById('w1');
 rad1.addEventListener('click',function(){
-document.getElementById('k2').className = "k2n" ;   
+document.getElementById('k2').className = "k2n" ;
+ctyp = 'b';
 });
 var rad1 =document.getElementById('w2');
 rad1.addEventListener('click',function(){
-document.getElementById('k2').className = "k2c" ;   
+document.getElementById('k2').className = "k2z" ; 
+ctyp = 'g'; 
 });
 var rad1 =document.getElementById('w3');
 rad1.addEventListener('click',function(){
-document.getElementById('k2').className = "k2z" ;   
+document.getElementById('k2').className = "k2o" ;  
+ctyp = 'o';
 });
